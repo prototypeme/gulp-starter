3785a5a0e9f61cf7d124670e082ee124a0475bbf
Murali Ramachari <Murali Ramachari>
2016-06-19 10:07:31 -0500

    Added more tasks and packages

---
+++ gulp/style.js
@@ -0,0 +1,16 @@
+var sass        = require('gulp-sass');
+var cleanCss    = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src('src/**/*.scss')
+        .pipe(sass({
+          noCache: true,
+          style: 'compact'
+        }))
+        .pipe(concatCss('index.css'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
-var requireDir = require('require-dir')
+var requireDir = require('require-dir');
+var browserSync = require('browser-sync').create();
 
 // Require all tasks in gulpfile.js/gulp, including subfolders
-requireDir('./gulp', { recurse: true })
+requireDir('./gulp', { recurse: true });
+
+var dirs = {
+  'src': 'src',
+  'dist': 'dist',
+  'tmp': 'tmp'
+};
+
+gulp.task('default', ['serve']);
